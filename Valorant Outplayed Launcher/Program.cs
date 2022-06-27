21ac6e136a189bcd69d25d9ae28e8b1c4a6b0f9b
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2022-06-27 11:51:00 +0100

    Add project files.

---
+++ Valorant Outplayed Launcher/Program.cs
@@ -0,0 +1,45 @@
+﻿// Very lazy program. Just made to fit purpose.
+string location = Directory.GetCurrentDirectory() + @"\Location.txt";
+string locationValorant = "";
+string locationOutplayed = "";
+string configValorant = "";
+string configOutplayed = "";
+
+
+// Load file stuff
+try {
+    string[] launchSettings = File.ReadAllLines(location);
+    locationValorant = launchSettings[0].Substring(launchSettings[0].IndexOf(':') + 1).Trim();
+    locationOutplayed = launchSettings[1].Substring(launchSettings[1].IndexOf(':') + 1).Trim();
+    configValorant = launchSettings[2].Substring(launchSettings[2].IndexOf(':') + 1).Trim();
+    configOutplayed = launchSettings[3].Substring(launchSettings[3].IndexOf(':') + 1).Trim();
+}
+catch {
+    if (!Directory.Exists(location)) 
+        File.WriteAllText(location, "Valorant: \nOutplayed: \nValorantArgs: \nOutplayedArgs: ");
+    
+    Console.WriteLine($"Please go to {location} and fill out the details there.");
+    Console.ReadLine();
+    Environment.Exit(0);
+}
+
+try {
+    Console.WriteLine("Launching Valorant...");
+    System.Diagnostics.Process.Start(locationValorant, configValorant);
+}
+catch {
+    Console.WriteLine("Valorant Failed to load. Press enter to exit...");
+    Console.ReadLine();
+    Environment.Exit(0);
+}
+try {
+    Console.WriteLine("Launching Outplayed...");
+    System.Diagnostics.Process.Start(locationOutplayed, configOutplayed);
+    Console.WriteLine("Success");
+    Environment.Exit(0);
+}
+catch {
+    Console.WriteLine("Outplayed Failed to load. Press enter to exit...");
+    Console.ReadLine();
+    Environment.Exit(0);
+}
